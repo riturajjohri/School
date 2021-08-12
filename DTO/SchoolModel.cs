52529a897b0ff7e8759c497b108ddb9d9e154c1c
riturajjohri <riturajjohri@gmail.com>
2021-08-12 21:58:36 +0530

    upload project

---
+++ DTO/SchoolModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DTO
+{
+    public class SchoolModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string Addresss { get; set; }
+
+        public string Classs { get; set; }
+
+        public string Section { get; set; }
+        public string BloodGrup { get; set; }
+        public string Mobile_no { get; set; }
+
+    }
+}
---
+++ Repo/EntityModel/SchoolModelDbModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.EntityModel
+{
+    public class SchoolModelDbModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string Addresss { get; set; }
+
+        public string Classs { get; set; }
+
+        public string Section { get; set; }
+        public string BloodGrup { get; set; }
+        public string Mobile_no { get; set; }
+    }
+}
